d74659a87588a7087548d1f9e3f0c3e5f51121e4
devgolol <yuumi7174@gmail.com>
2025-10-12 20:54:02 +0900

    ENTITY ver2

---
+++ GameServer/Models/GameLog.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameServer.Models
+{
+    public class GameLog
+    {
+        [Key]
+        public int LogId { get; set; }
+        public int PlayerId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Action { get; set; }
+        public string Details { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public string IpAddress { get; set; }
+
+        public Player Player { get; set; }
+    }
+}
---
+++ GameServer/Models/UserReward.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameServer.Models
+{
+    public class UserReward
+    {
+        [Key]
+        public int RewardId { get; set; }
+        public int PlayerId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string RewardType { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string RewardName { get; set; }
+        public int Quantity { get; set; } = 1;
+        public DateTime AcquiredAt { get; set; } = DateTime.UtcNow;
+        public bool IsCollected { get; set; } = false;
+        public string Description { get; set; }
+        public Player Player { get; set; }
+    }
+}
